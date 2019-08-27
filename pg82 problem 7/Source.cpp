cb62ffe954cd90d28da58d6ec6b2ddab53cda4d0
JSG1987 <JGentry0058@gmail.com>
2019-08-27 14:36:47 -0500

    Added something new

--- pg82 problem 7/Source.cpp
+++ pg82 problem 7/Source.cpp
@@ -11,9 +11,9 @@ int main()
 	float fiveYear = oceanLVL * 5;
 	float sevenYear = oceanLVL * 7;
 	float tenYear = oceanLVL * 10;
-	cout << "after 5 years the ocean level is " << fiveYear << endl;
-	cout << "After 7 years the ocean level is " << sevenYear << endl;
-	cout << "After 10 years the ocean level is " << tenYear << endl;
+	cout << "after 5 years the ocean level is " << fiveYear <<"mm"<< endl;
+	cout << "After 7 years the ocean level is " << sevenYear << "mm" << endl;
+	cout << "After 10 years the ocean level is " << tenYear << "mm" << endl;
 
 
 
