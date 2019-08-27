3b8162fb65eddf48b7de883d7aea93209545af2b
JSG1987 <JGentry0058@gmail.com>
2019-08-27 14:35:10 -0500

    HW7

---
+++ pg82 problem 7/Source.cpp
@@ -0,0 +1,22 @@
+//The goal of this program is to calculate ocean levels by year
+
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+	float oceanLVL = 1.5;
+	float fiveYear = oceanLVL * 5;
+	float sevenYear = oceanLVL * 7;
+	float tenYear = oceanLVL * 10;
+	cout << "after 5 years the ocean level is " << fiveYear << endl;
+	cout << "After 7 years the ocean level is " << sevenYear << endl;
+	cout << "After 10 years the ocean level is " << tenYear << endl;
+
+
+
+	system("PAUSE");
+	return 0; 
+}
